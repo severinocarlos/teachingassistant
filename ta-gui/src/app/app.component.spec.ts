2b63d83b52ad3c84eaaf7afe3e848ef74c600c6d
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:03:47 -0300

    elementos do formulario para cadastro de alunos
    add: register students

--- ta-gui/src/app/app.component.spec.ts
+++ ta-gui/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ta-gui'`, () => {
+  it(`should have as title 'roteiro-ess'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ta-gui');
+    expect(app.title).toEqual('roteiro-ess');
   });
 
   it('should render title', () => {
