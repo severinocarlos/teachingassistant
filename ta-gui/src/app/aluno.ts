7a70aa700fe98a308e9db511ed5f5d3ab22eced4
severinocarlos <juninho_456@hotmail.com>
2023-03-19 20:23:14 -0300

    fix: bug dic metas

--- ta-gui/src/app/aluno.ts
+++ ta-gui/src/app/aluno.ts
@@ -2,7 +2,7 @@ export class Aluno {
   nome: string = '';
   cpf: string = '';
   email: string = '';
-  metas: Map<string,string>;
+  metas: Map<string,string> = new Map<string,string>();
 
   constructor() {
     this.clean();
@@ -12,7 +12,7 @@ export class Aluno {
     this.nome = "";
     this.cpf = "";
     this.email = "";
-    this.metas = new Map<string,string>();
+    this.metas.clear();
   }
 
   clone(): Aluno {
@@ -25,9 +25,9 @@ export class Aluno {
   }
 
   cloneMetas(): Map<string,string> {
-    var metas: Map<string,string> = new Map<string,string>();
-    for (let key in this.metas) {
-      metas[key] = this.metas[key];
+    const metas: Map<string,string> = new Map<string,string>();
+    for (let [key, value] of this.metas) {
+      metas.set(key, value);
     }
     return metas;
   }
